ac89c0287ffab7d359f5b9357e5612c3361f219c
mark-HFUT <1546662938@qq.com>
2023-04-25 19:47:52 +0800

    ''

---
+++ backend/nlp/translate.py
@@ -0,0 +1,31 @@
+# 导入requests库
+import requests
+import hashlib
+url = "http://api.fanyi.baidu.com/api/trans/vip/translate"
+appid = "20230424001654753"
+salt = '42'
+key = 'egbame2d6QRlW362pFjQ'
+def translate(text, target_lang):
+    
+    md5 = hashlib.md5()
+    sign = appid + text + salt + key
+    sign = sign.encode('utf-8')
+    md5.update(sign)
+    sign = md5.hexdigest()
+
+    params = {
+        "q": text,
+        "from": "auto",
+        "to": target_lang,
+        "appid": appid,
+        "salt": salt,
+        "sign": sign
+    }
+
+    response = requests.get(url, params=params)
+
+    result = response.json()
+    if "trans_result" in result.keys():
+        return result["trans_result"][0]["dst"]
+    else:
+        return ''
--- backend/nlp/views.py
+++ backend/nlp/views.py
@@ -1,10 +1,71 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import pysbd
+from deepmultilingualpunctuation import PunctuationModel
+from .translate import translate
+import threading
+import time
+import nltk
+
+# The default language is 'english'
 # Create your views here.
+seg_chn = pysbd.Segmenter(language="zh", clean=True)
+seg_eng = pysbd.Segmenter(language="en", clean=True)
+punc = PunctuationModel()
+buf = []
+
+time_out_counter = 0
+
+def translate_thread():
+    global buf, time_out_counter
+    send = ''
+    while True:
+        if buf:
+            print("buf:", buf)
+            snt = ' '.join(buf)
+            snt = punc.restore_punctuation(snt)
+            print("sentence:", snt)
+            seg = seg_eng.segment(snt)
+            print("sbd:", seg)
+            if len(seg) > 1 or time_out_counter > 3 and len(seg) > 0:
+                send = seg.pop(0)
+                n_words = len(nltk.word_tokenize(send)) # 使用nltk分词
+                while buf and n_words > 0:
+                    buf.pop(0)
+                    n_words -= 1
+            if send:
+                res = translate(send, 'en')
+                print("response:", res)
+        print('-----------------------------------------')
+        time.sleep(1)
+        time_out_counter += 1
+
+# translator = threading.Thread(target=translate_thread, daemon=True)
+# print('start translator')
+# translator.start()
 
 @api_view(['POST'])
 def speech(request):
+    global buf, time_out_counter
     word = request.data.get('word')
-    print(word)
-    return Response('backend: '+word)
+    buf.append(word)
+    send = ''
+    if buf:
+        print("buf:", buf)
+        snt = ''.join(buf)
+        snt = punc.restore_punctuation(snt)
+        print("sentence:", snt)
+        seg = seg_chn.segment(snt)
+        print("sbd:", seg)
+        if len(seg) > 1 or time_out_counter > 3 and len(seg) > 0:
+            send = seg.pop(0)
+            n_words = len(nltk.word_tokenize(send)) # 使用nltk分词
+            while buf and n_words > 0:
+                buf.pop(0)
+                n_words -= 1
+        if send:
+            res = translate(send, 'en')
+            print("response:", res)
+            return Response(res)
+    return Response('')
