b888b22c1010fb00f0dd3b1288f09cf16b8b92ca
mark-HFUT <1546662938@qq.com>
2023-04-26 13:26:37 +0800

    'trans'

--- backend/nlp/views.py
+++ backend/nlp/views.py
@@ -3,18 +3,21 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import pysbd
 from deepmultilingualpunctuation import PunctuationModel
+from .zh_punct import zh_punc
 from .translate import translate
 import threading
 import time
 import nltk
+import spacy
 
 # The default language is 'english'
 # Create your views here.
-seg_chn = pysbd.Segmenter(language="zh", clean=True)
-seg_eng = pysbd.Segmenter(language="en", clean=True)
-punc = PunctuationModel()
+seg_chn = pysbd.Segmenter(language="zh", clean=False)
+seg_eng = pysbd.Segmenter(language="en", clean=False)
 buf = []
-
+nlp = spacy.blank('zh')
+nlp.add_pipe('sentencizer')
+punc = PunctuationModel()
 time_out_counter = 0
 
 def translate_thread():
@@ -24,7 +27,7 @@ def translate_thread():
         if buf:
             print("buf:", buf)
             snt = ' '.join(buf)
-            snt = punc.restore_punctuation(snt)
+            snt = punc.restore_punc(snt)
             print("sentence:", snt)
             seg = seg_eng.segment(snt)
             print("sbd:", seg)
@@ -45,27 +48,26 @@ def translate_thread():
 # print('start translator')
 # translator.start()
 
+def sbd(buf, lang):
+    if lang == 'zh':
+        snt = ''.join(buf)
+    elif lang == 'en':
+        snt = ' '.join(buf)
+        snt = punc.restore_punc(snt)
+        seg = seg_eng.segment(snt)
+        seg = nlp(snt)
+        seg = [sent.text for sent in seg.sents]
+        return seg
+    
+        
+
 @api_view(['POST'])
 def speech(request):
-    global buf, time_out_counter
-    word = request.data.get('word')
-    buf.append(word)
-    send = ''
-    if buf:
-        print("buf:", buf)
-        snt = ''.join(buf)
-        snt = punc.restore_punctuation(snt)
-        print("sentence:", snt)
-        seg = seg_chn.segment(snt)
-        print("sbd:", seg)
-        if len(seg) > 1 or time_out_counter > 3 and len(seg) > 0:
-            send = seg.pop(0)
-            n_words = len(nltk.word_tokenize(send)) # 使用nltk分词
-            while buf and n_words > 0:
-                buf.pop(0)
-                n_words -= 1
-        if send:
-            res = translate(send, 'en')
-            print("response:", res)
-            return Response(res)
+    # global buf, time_out_counter
+    words = request.data.get('words')
+    if words:
+        print(words)
+        res = translate(words, 'en')
+        print("response:", res)
+        return Response(res)
     return Response('')
