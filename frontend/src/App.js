786cbe81f0bf66dafb4be5424ab51df2ad24af96
Zhang Zhimu <57898127+nitianmu02@users.noreply.github.com>
2023-05-21 00:53:48 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Chinese from './pages/chinese'
 import English2 from './pages/english2'
 import Chinese2 from './pages/chinese2'
 import Start from './pages/start';
-import Test from './pages/test'
+
 
 function App() {
   // return (
@@ -24,7 +24,6 @@ function App() {
           <Route path="chinese" element = {<Chinese/>}/>
           <Route path="english2" element = {<English2/>}/>
           <Route path="chinese2" element = {<Chinese2/>}/>
-          <Route path="test" element = {<Test/>}/>
       </Routes>
     </HashRouter>
   )
